d9339b21b1ea333804f28609346380ab286ebdc2
PC14 <daniel@vallegrande.edu.pe>
2018-05-26 10:17:32 -0500

    Estoy subiendo en excel el listado de las tablas de mi base de datos

--- Lab01/QueryOne.sql
+++ Lab01/QueryOne.sql
@@ -7,3 +7,13 @@ GO
 SELECT database_id, name 
 FROM sys.databaseS
 GO
+
+/* Poner en uso una base de datos */
+
+USE HospitalHHH
+GO
+
+/* Ver el listado de tablas de una base de datos */
+USE AdventureWorks2016
+SELECT name FROM sys.tables
+GO
