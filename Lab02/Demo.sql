005bb9fd440a57ff4449978c787756320b35d7b8
PC14 <daniel@vallegrande.edu.pe>
2018-05-29 11:15:51 -0500

    Lab02 creado

---
+++ Lab02/Demo.sql
@@ -0,0 +1,40 @@
+USE AdventureWorks2016
+
+select * from Person.Address
+
+/* AND */
+SELECT ModifiedDate, City FROM Person.Address
+WHERE YEAR(ModifiedDate) = '2014' AND 
+        MONTH (ModifiedDate) = '01' AND City = 'Beaverton'
+
+/* LIKE */
+SELECT AddressLine2 FROM Person.Address
+WHERE AddressLine2 like 'B%'
+
+/* NOT */
+SELECT * FROM HumanResources.Employee WHERE NOT Gender = 'M'
+
+/* IN */
+SELECT BusinessEntityID, FirstName, LastName
+FROM Person.Person
+WHERE FirstName IN ('Michael')
+
+/* AND - OR - LIKE*/
+SELECT BusinessEntityID, JobTitle
+FROM HumanResources.Employee
+WHERE BusinessEntityID >= '10'
+AND BusinessEntityID <= '40'
+OR JobTitle like 'V%'
+
+
+
+
+
+
+
+
+
+
+create view d
+as select FirstName, PhoneNumber
+FROM
